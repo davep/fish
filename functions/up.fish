a3c9ae8363b937fc18812644cbef23821de75537
Dave Pearson <dave.pearson@synpromics.com>
2019-05-06 16:56:44 +0100

    :sparkles: Add 'up'

---
+++ functions/up.fish
@@ -0,0 +1,7 @@
+##############################################################################
+# Handy command to move up a single directory.
+function up -d "Move up one directory"
+    cd ..
+end
+
+### up.fish ends here
