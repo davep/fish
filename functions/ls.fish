884cacdf9bf2d859e18958f8c4199a29ee509e3b
Dave Pearson <davep@davep.org>
2019-05-06 20:21:55 +0100

    :sparkles: Ensure ls works well on macOS too

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,7 +1,12 @@
 ##############################################################################
 # Set some base options for the ls command.
-function ls -d "Base ls command with colour added"
-    /bin/ls -F --color $argv
+function ls -d "Base ls command with extras added"
+    switch (uname)
+        case "Darwin"
+            /bin/ls -F $argv
+        case "*"
+            /bin/ls -F --color $argv
+    end
 end
 
 ### ls.fish ends here
