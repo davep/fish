d4bcf813442723cb6fe8328b0a1b6f3002b822ba
Dave Pearson <dave.pearson@synpromics.com>
2019-05-06 13:07:30 +0100

    :lipstick: More prompt changes to make them more zsh-a-like

--- config.fish
+++ config.fish
@@ -1,6 +1,13 @@
 # Prompt colours
 set -g color_dir_bg 3a3a3a
 set -g color_dir_str white
+set -g color_user_bg 262626
+set -g color_user_str 767676
+
+# Prompt contents
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g fish_prompt_pwd_dir_length 0
 
 # General handy aliases.
 alias vdir=dir
