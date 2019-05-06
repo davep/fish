5244981bba5cd65a911d2a798fc51f2dcf416c33
Dave Pearson <dave.pearson@synpromics.com>
2019-05-06 17:22:32 +0100

    :bulb: Add some extra documentation to the ls command

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,3 +1,7 @@
+##############################################################################
+# Set some base options for the ls command.
 function ls -d "Base ls command with colour added"
     /bin/ls -F --color $argv
 end
+
+### ls.fish ends here
