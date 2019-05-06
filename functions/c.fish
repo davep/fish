900add29fb0e24984064a524bc87b1fe8c85c65c
Dave Pearson <dave.pearson@synpromics.com>
2019-05-06 11:02:42 +0100

    :sparkles: Add a shorthand for clearing the terminal

---
+++ functions/c.fish
@@ -0,0 +1,7 @@
+##############################################################################
+# Simple command to clear the terminal.
+function c --description "Clear the terminal"
+    clear
+end
+
+### c.fish ends here
