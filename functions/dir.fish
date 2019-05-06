8a28ac931608294fec79d34ec9336f749a436e92
Dave Pearson <dave.pearson@synpromics.com>
2019-05-06 17:21:48 +0100

    :bulb: Add some comments to the dir command

--- functions/dir.fish
+++ functions/dir.fish
@@ -1,3 +1,7 @@
+##############################################################################
+# Have dir show a vertical directory listing.
 function dir -d "Vertical directory listing"
     ls -l
 end
+
+### dir.fish ends here
