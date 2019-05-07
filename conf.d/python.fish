78df39c577b8bcb9c0992e02f973f901a41e5654
Dave Pearson <dave.pearson@synpromics.com>
2019-05-07 09:15:29 +0100

    :hammer: Be more fishy

--- conf.d/python.fish
+++ conf.d/python.fish
@@ -2,12 +2,12 @@
 # Set up things to do with Python.
 
 # Don't let pipenv mess with my prompt
-export VIRTUAL_ENV_DISABLE_PROMPT=yes
+set -x VIRTUAL_ENV_DISABLE_PROMPT yes
 
 # Have pipenv always keep the venv in the project directory.
-export PIPENV_VENV_IN_PROJECT=true
+set -x PIPENV_VENV_IN_PROJECT true
 
 # Ensure that Python uses an rc file when we start the repl.
-export PYTHONSTARTUP=~/.pythonrc
+set -x PYTHONSTARTUP ~/.pythonrc
 
 ### pipenv.fish ends here
