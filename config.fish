1ae1f57c9f7caa4e41b1e65d1d029ac1d2d397b4
Dave Pearson <dave.pearson@synpromics.com>
2019-05-07 09:12:55 +0100

    :sparkles: Add some general settings

--- config.fish
+++ config.fish
@@ -1,3 +1,10 @@
-# Nothing to see here at the moment.
+##############################################################################
+# General settings.
+
+# Always use less as my paper.
+set -x PAGER less
+
+# Ensure we always speak British.
+set -x DICTIONARY british
 
 ### config.fish ends here
