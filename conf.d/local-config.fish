43c8a593214c2aa96de89c7bffd36e9bff368bb7
Dave Pearson <dave.pearson@synpromics.com>
2019-05-07 09:05:18 +0100

    :sparkles: Add support for some local configuration

---
+++ conf.d/local-config.fish
@@ -0,0 +1,12 @@
+##############################################################################
+# Load up any local config.
+#
+# Some things are specific to a machine, or specific to a location, and
+# don't really being in my central repository (think work-related stuff,
+# etc). So this code will load everything inside ~/.config/local_fish
+
+for local_conf in ~/.config/local-fish/*.fish
+    source $local_conf
+end
+
+### local-config.fish ends here
