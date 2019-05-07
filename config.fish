00f2c828b5a95e0e900477a08c2c9e13185d9d83
Dave Pearson <dave.pearson@synpromics.com>
2019-05-07 09:24:10 +0100

    :sparkles: Add the usual suspects to the PATH

--- config.fish
+++ config.fish
@@ -7,4 +7,9 @@ set -x PAGER less
 # Ensure we always speak British.
 set -x DICTIONARY british
 
+# Extra path items.
+set PATH ~/.cargo/bin $PATH
+set PATH ~/.local/bin $PATH
+set PATH ~/bin        $PATH
+
 ### config.fish ends here
