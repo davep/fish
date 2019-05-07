0cfc457fab881ca580ad204bff684b29fd0f741b
Dave Pearson <dave.pearson@synpromics.com>
2019-05-07 11:17:40 +0100

    :sparkles: Quickly visit github

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -1,6 +1,12 @@
-# General handy aliases.
+##############################################################################
+# Assorted handy aliases. Things that don't require a function really.
+
+# Directory listings.
 alias vdir=dir
 alias d=dir
 alias v=vdir
+
+# Visit github.
+alias gh="o https://github.com/"
 
 ### aliases.fish ends here
