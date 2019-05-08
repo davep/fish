023932f69669590d84415d3cbbcc39243ba0211a
Dave Pearson <dave.pearson@synpromics.com>
2019-05-08 15:14:43 +0100

    :sparkles: Hand-set syntax highlighting
    Rather than doing this via the fish_variables file, do it "by hand". This
    helps with having things work nice in 2.4 and 3.x, as far as I can tell.

---
+++ conf.d/syntax_highlighting.fish
@@ -0,0 +1,8 @@
+##############################################################################
+# Configure syntax highlighting.
+
+set -U fish_color_command 00ff00
+set -U fish_color_param 00bb00
+set -U fish_color_valid_path --underline
+
+### syntax_highlighting.fish ends here
