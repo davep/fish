1a0f530fc5c52f15aed149e43d9ca64942791274
Dave Pearson <dave.pearson@synpromics.com>
2019-05-14 16:13:48 +0100

    :sparkles: Make the Clojure repl nicer to use where I have it

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -17,6 +17,12 @@ alias m=more
 alias p2=python2
 alias p3=python3
 
+# If I have rlwrap and Clojure...
+if type -q rlwrap and type -q clojure
+    # ...have the Clojure repl work nicer.
+    alias clojure="rlwrap clojure"
+end
+
 # There seems to be a clash with /bin/open on some GNU/Linux boxes, and
 # fish's own open command. So if I'm on a system with a /bin/open, let's
 # simply alias it to xdg-open.
