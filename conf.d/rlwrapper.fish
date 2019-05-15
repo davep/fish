413c5a29cc83ed70df0479d32a0154928049a31c
Dave Pearson <dave.pearson@synpromics.com>
2019-05-15 17:07:01 +0100

    :sparkles: rlwrap Chicken Scheme's REPL

--- conf.d/rlwrapper.fish
+++ conf.d/rlwrapper.fish
@@ -5,6 +5,7 @@
 if type -fq rlwrap
     # ...wrap various REPLs if I also have them.
     type -fq clojure; and alias clojure="rlwrap clojure"
+    type -fq csi; and alias csi="rlwrap csi"
 end
 
 ### rlwrapper.fish ends here
