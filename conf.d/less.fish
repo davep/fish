7c90af3e0590a8f21709c1e9b1dd04b09b1221f7
Dave Pearson <dave.pearson@synpromics.com>
2019-05-20 11:49:56 +0100

    :sparkles: Syntax highlight in less, when possible

--- conf.d/less.fish
+++ conf.d/less.fish
@@ -3,8 +3,17 @@
 
 set -x LESS MMiqC
 
+# If we've got lesspipe...
 if test -x /usr/bin/lesspipe
+    # ...use that.
     set -x LESSOPEN "|/usr/bin/lesspipe %s"
+end
+
+# On the other hand, if we've got hilite...
+if test -x /usr/share/source-highlight/src-hilite-lesspipe.sh
+    # ...use that instead.
+    set -x LESS "$LESS"R
+    set -x LESSOPEN "|/usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 end
 
 ### less.fish ends here
