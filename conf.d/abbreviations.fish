9d58fee6cb1941984128a18677d0d5d9e5b885ab
Dave Pearson <dave.pearson@synpromics.com>
2019-11-04 15:19:13 +0000

    :sparkles: Add pylintshame
    Little abbreviation that helps me find out what I've told pylint to ignore
    within Python code. Could be useful for deciding what to improve as time
    goes on.

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -27,6 +27,7 @@ end
 # Python things
 abbr -g p2 python2
 abbr -g p3 python3
+abbr -g pylintshame "rg --no-messages \"pylint:disable=\" | awk 'BEGIN{FS=\"disable=\";}{print \$2}' | tr \",\" \"\n\" | sort | uniq -c | sort -hr"
 
 # Git things
 abbr -g gp "git pull"
