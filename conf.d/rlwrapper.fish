dd2721e0565928b4145698a42c9c74e4534c578a
Dave Pearson <dave.pearson@synpromics.com>
2019-11-04 16:58:18 +0000

    :sparkles: Add a "lisp" alias
    This uses roswell to run the current Lisp of choice, wrapping it with
    rlwrap. This does mean that both roswell and rlwrap need to be installed.

--- conf.d/rlwrapper.fish
+++ conf.d/rlwrapper.fish
@@ -6,6 +6,7 @@ if type -fq rlwrap
     # ...wrap various REPLs if I also have them.
     type -fq clojure; and alias clojure="rlwrap clojure"
     type -fq csi; and alias csi="rlwrap csi"
+    type -fq ros; and alias lisp="rlwrap ros run"
 end
 
 ### rlwrapper.fish ends here
