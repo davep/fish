bd13bac493ceecb100f0e27773556b39e8b85561
Dave Pearson <dave.pearson@synpromics.com>
2019-11-04 10:12:01 +0000

    :sparkles: Add some Homebrew config

---
+++ conf.d/homebrew.fish
@@ -0,0 +1,14 @@
+##############################################################################
+# Things to make a Homebrew environment nicer.
+
+# If we're on macOS...
+if string match (uname) "Darwin" > /dev/null
+
+    # Ensure openssl library directory is in the LDFLAGS path.
+    if test -d /usr/local/opt/openssl/lib/
+        set LDFLAGS /usr/local/opt/openssl/lib/ $LDFLAGS
+    end
+
+end
+
+### homebrew.fish ends here
