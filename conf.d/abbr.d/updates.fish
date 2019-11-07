d95b8a71a44c60801b05b661f3b19684d2a686ff
Dave Pearson <dave.pearson@synpromics.com>
2019-11-07 10:09:42 +0000

    :zap: Improve how I create the update/grade commands.

--- conf.d/abbr.d/updates.fish
+++ conf.d/abbr.d/updates.fish
@@ -1,26 +1,33 @@
 ##############################################################################
 # Abbreviations related to installing and upgrading things.
 
+# Locals.
+set -l upd ""
+set -l upg ""
+
+# Decide which commands to use depending on environment.
 switch (uname)
     case "Linux"
-        abbr -g upd "sudo apt update"
-        abbr -g upg "sudo apt upgrade; sudo apt autoremove; sudo apt autoclean"
+        set upd "sudo apt update"
+        set upg "sudo apt upgrade; sudo apt autoremove; sudo apt autoclean"
     case "Darwin"
-        abbr -g upd "brew update"
-        abbr -g upg "brew upgrade; brew cleanup"
+        set upd "brew update"
+        set upg "brew upgrade; brew cleanup"
         abbr -g bi "brew install"
         abbr -g bu "brew uninstall"
         abbr -g bs "brew search"
-end
-
-# A "do all" updating/upgrading abbreviation.
-if test "$_fish_abbr_upd"
-    abbr -g upall "$_fish_abbr_upd; $_fish_abbr_upg"
+    case "*"
+        exit
 end
 
 # I use rust in more than one place, so if cargo exists...
 if type -q cargo
-    abbr -g cargup "cargo install-update -a"
+    set upg "$upg; cargo install-update -a"
 end
+
+# Create the aliases for doing updates and upgrades.
+abbr -g upd "$upd"
+abbr -g upg "$upg"
+abbr -g upall "$upd; $upg"
 
 ### updates.fish ends here
