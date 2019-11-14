bb08e6de1ec80ba15e5ca5ec989a0d5bdfe61fb6
Dave Pearson <dave.pearson@synpromics.com>
2019-11-14 12:45:39 +0000

    :sparkles: Set LIBRARY_PATH as well as LDFLAGS for opensel

--- conf.d/homebrew.fish
+++ conf.d/homebrew.fish
@@ -4,9 +4,10 @@
 # If we're on macOS...
 if string match -q (uname) "Darwin"
 
-    # Ensure openssl library directory is in the LDFLAGS path.
+    # Ensure openssl library directory is seen by things.
     if test -d /usr/local/opt/openssl/lib/
         set LDFLAGS /usr/local/opt/openssl/lib/ $LDFLAGS
+        set LIBRARY_PATH /usr/local/opt/openssl/lib/ $LIBRARY_PATH
     end
 
 end
