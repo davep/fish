3831b9acbac35dc6afa4c593cb58636a3975ccd2
Dave Pearson <dave.pearson@synpromics.com>
2019-11-14 09:45:53 +0000

    :sparkles: Add a couple of WiFi-oriented macOS abbreviations

--- conf.d/abbr.d/macOS.fish
+++ conf.d/abbr.d/macOS.fish
@@ -7,6 +7,10 @@ if string match -q (uname) "Darwin"
     # When was this mac "born?"
     abbr -g macdob 'stat -f "%Sa" /var/db/.AppleSetupDone'
 
+    # Show some information about current WiFi
+    abbr -g wifi "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --info"
+    abbr -h wifils "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --scan"
+
 end
 
 ### macOS.fish ends here
