b4219414da85a77ea1f86b56f8a0ba61d6312e16
Dave Pearson <dave.pearson@synpromics.com>
2019-11-19 11:19:17 +0000

    :sparkles: Add MySQL client support

--- conf.d/homebrew.fish
+++ conf.d/homebrew.fish
@@ -6,8 +6,15 @@ if string match -q (uname) "Darwin"
 
     # Ensure openssl library directory is seen by things.
     if test -d /usr/local/opt/openssl/lib/
-        set LDFLAGS /usr/local/opt/openssl/lib/ $LDFLAGS
-        set LIBRARY_PATH /usr/local/opt/openssl/lib/ $LIBRARY_PATH
+        set LDFLAGS "-L/usr/local/opt/openssl/lib/" $LDFLAGS
+        set LIBRARY_PATH "/usr/local/opt/openssl/lib/" $LIBRARY_PATH
+    end
+
+    # Ensure MySQL client library is seen by things.
+    if test -d /usr/local/opt/mysql-client/
+        set LDFLAGS "-L/usr/local/opt/mysql-client/lib" $LDFLAGS
+        set CPPFLAGS "-I/usr/local/opt/mysql-client/include" $CPPFLAGS
+        set PKG_CONFIG_PATH "/usr/local/opt/mysql-client/lib/pkgconfig" $PKG_CONFIG_PATH
     end
 
 end
