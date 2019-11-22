121b908b315a5d298df1eeacfaa712bec0a368b6
Dave Pearson <dave.pearson@synpromics.com>
2019-11-22 14:38:58 +0000

    :hammer: Further improve how we set up some Homebrew stuff

--- conf.d/homebrew.fish
+++ conf.d/homebrew.fish
@@ -7,19 +7,19 @@ if string match -q (uname) "Darwin"
     # Ensure openssl library directory is seen by things.
     if test -d "/usr/local/opt/openssl@1.1/"
         add_path_maybe "/usr/local/opt/openssl@1.1/bin"
-        set -gx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib/" $LDFLAGS
-        set -gx LIBRARY_PATH "/usr/local/opt/openssl@1.1/lib/" $LIBRARY_PATH
-        set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include" $CPPFLAGS
-        set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig" $PKG_CONFIG_PATH
+        set -agx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib/"
+        set -agx LIBRARY_PATH "/usr/local/opt/openssl@1.1/lib/"
+        set -agx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
+        set -agx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
     end
 
     # Ensure MySQL client library is seen by things.
     if test -d "/usr/local/opt/mysql-client/"
         add_path_maybe "/usr/local/opt/mysql-client/bin"
-        set -gx LDFLAGS "-L/usr/local/opt/mysql-client/lib" $LDFLAGS
-        set -gx LIBRARY_PATH "/usr/local/opt/mysql-client/lib" $LIBRARY_PATH
-        set -gx CPPFLAGS "-I/usr/local/opt/mysql-client/include" $CPPFLAGS
-        set -gx PKG_CONFIG_PATH "/usr/local/opt/mysql-client/lib/pkgconfig" $PKG_CONFIG_PATH
+        set -agx LDFLAGS "-L/usr/local/opt/mysql-client/lib"
+        set -agx LIBRARY_PATH "/usr/local/opt/mysql-client/lib"
+        set -agx CPPFLAGS "-I/usr/local/opt/mysql-client/include"
+        set -agx PKG_CONFIG_PATH "/usr/local/opt/mysql-client/lib/pkgconfig"
     end
 
 end
