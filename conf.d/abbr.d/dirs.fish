4dd9ef794b5d7f4ccb56f77cdafd67f4f6a6c1eb
Dave Pearson <dave.pearson@synpromics.com>
2019-12-13 15:26:23 +0000

    :zap: Restore "a" as an abbreviation for "ls -a"
    Well, not so much restore it as an abbreviation, as it never was one, but
    it's an alias I've used in the past and it should live on.

--- conf.d/abbr.d/dirs.fish
+++ conf.d/abbr.d/dirs.fish
@@ -1,6 +1,7 @@
 ##############################################################################
 # Directory listing and movement abbreviations.
 abbr -g d ls
+abbr -g a "ls -a"
 abbr -g da "ls -a"
 abbr -g v "ls -l"
 abbr -g va "ls -la"
