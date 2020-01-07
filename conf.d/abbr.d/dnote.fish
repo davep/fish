d361d632d38610cbc46ad35450b33eef911561a9
Dave Pearson <dave.pearson@synpromics.com>
2020-01-07 10:22:31 +0000

    :sparkles: Start an abbreviation file for dnote

---
+++ conf.d/abbr.d/dnote.fish
@@ -0,0 +1,11 @@
+##############################################################################
+# Abbreviations for working with dnote.
+
+# If we have dnote installed...
+if type -q dnote
+    abbr -g dn dnote
+    abbr -g dna "dnote add"
+    abbr -g dnf "dnote find"
+end
+
+### dnote.fish ends here
