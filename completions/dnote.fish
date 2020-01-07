d575d03308355b7c4d63ae91b2ca4cbb0a3cc727
Dave Pearson <dave.pearson@synpromics.com>
2020-01-07 15:09:56 +0000

    :sparkles: Guard against entering multiple commands

--- completions/dnote.fish
+++ completions/dnote.fish
@@ -1,19 +1,22 @@
 # TODO: This is far from complete (no pun...). I need to properly wrap my
 # head around how custom completions actually work.
 
+# List of commands.
+set -l dnote_commands add edit find help login logout remove sync version
+
 # Start clean.
 complete -c dnote --erase
 
 # Add completion for the main commands.
-complete -c dnote -fa add     -d "Add a new note"
-complete -c dnote -fa edit    -d "Edit a note or a book"
-complete -c dnote -fa find    -d "Find notes by keywords"
-complete -c dnote -fa help    -d "Help about any command"
-complete -c dnote -fa login   -d "Login to dnote server"
-complete -c dnote -fa logout  -d "Logout from the server"
-complete -c dnote -fa remove  -d "Remove a note or a book"
-complete -c dnote -fa sync    -d "Sync data with the server"
-complete -c dnote -fa version -d "Print the version number of Dnote"
-complete -c dnote -fa view    -d "List books, notes or view a content"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa add     -d "Add a new note"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa edit    -d "Edit a note or a book"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa find    -d "Find notes by keywords"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa help    -d "Help about any command"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa login   -d "Login to dnote server"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa logout  -d "Logout from the server"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa remove  -d "Remove a note or a book"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa sync    -d "Sync data with the server"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa version -d "Print the version number of Dnote"
+complete -c dnote -n "not __fish_seen_subcommand_from $dnote_commands" -fa view    -d "List books, notes or view a content"
 
 ### dnote.fish ends here
