99a8d1cc4a4b589852552a6a85e50aa0b07b0b28
Dave Pearson <dave.pearson@synpromics.com>
2020-02-18 11:35:20 +0000

    :sparkles: Add a macOS command for flushing DNS.

--- conf.d/abbr.d/macOS.fish
+++ conf.d/abbr.d/macOS.fish
@@ -11,6 +11,9 @@ if string match -q (uname) "Darwin"
     abbr -g wifi "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo"
     abbr -g wifils "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --scan"
 
+    # Flush the DNS cache.
+    abbr -g flushdns "sudo killall -HUP mDNSResponder"
+
 end
 
 ### macOS.fish ends here
