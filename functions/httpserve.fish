2c3dcb6ad13c9c363e1a2ef14e58264339834c0c
Dave Pearson <dave.pearson@synpromics.com>
2020-02-26 15:19:39 +0000

    :hammer: Simplify how we serve
    This removes the opening of a web browser, it's easy enough for me to open
    one myself and point it in the right direction. Also, because why not, the
    port is now chosen randomly, within a sensible range.

--- functions/httpserve.fish
+++ functions/httpserve.fish
@@ -5,9 +5,7 @@ function httpserve -d "Very simple web server for the current working directory.
     echo ============================================================
     echo Serving (pwd)
     echo ============================================================
-    python3 -m http.server 8765 &
-    open http://127.0.0.1:8765
-    fg
+    python3 -m http.server (random 8100 8900)
 end
 
 ### httpserve.fish ends here
