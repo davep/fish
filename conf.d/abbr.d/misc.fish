4e011279587d72631f9924f4221e7d5018430a9a
Dave Pearson <dave.pearson@synpromics.com>
2020-03-12 11:59:23 +0000

    :sparkles: Add a psgrep abbreviation
    This has been a function/alias that's followed me around for years. It's a
    bit clunky but it's useful in most cases. And yet, for some reason, I've not
    actually recreated it in fish.
    
    I have now.

--- conf.d/abbr.d/misc.fish
+++ conf.d/abbr.d/misc.fish
@@ -6,6 +6,7 @@ abbr -g greedy "du -hs * | sort -rh"
 abbr -g cmdfreq "history | cut -d \" \" -f 1 | sort | uniq -c | sort -rh"
 abbr -g fcfg "cd ~/.config/fish"
 abbr -g flcfg "cd ~/.config/local-fish"
+abbr -g psgrep "ps auxwww | grep"
 
 # Make using https://github.com/jarun/googler shorter.
 if type -q googler
