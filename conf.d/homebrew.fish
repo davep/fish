161512dfe31b4ec7efe11d2cb4c93a7bce9c2c3b
Dave Pearson <dave.pearson@synpromics.com>
2020-07-15 11:39:01 +0100

    :hammer: Reduce some duplication of effort

--- conf.d/homebrew.fish
+++ conf.d/homebrew.fish
@@ -5,12 +5,13 @@
 if string match -q (uname) "Darwin"
 
     # Ensure openssl library directory is seen by things.
-    if test -d "/usr/local/opt/openssl@1.1/"
-        add_path_maybe "/usr/local/opt/openssl@1.1/bin"
-        set -agx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib/"
-        set -agx LIBRARY_PATH "/usr/local/opt/openssl@1.1/lib/"
-        set -agx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
-        set -agx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
+    set -l ossldir "/usr/local/opt/openssl@1.1"
+    if test -d $ossldir
+        add_path_maybe "$ossldir/bin"
+        set -agx LDFLAGS "-L$ossldir/lib/"
+        set -agx LIBRARY_PATH "$ossldir/lib/"
+        set -agx CPPFLAGS "-I$ossldir/include"
+        set -agx PKG_CONFIG_PATH "$ossldir/lib/pkgconfig"
     end
 
     # Ensure MySQL client library is seen by things.
