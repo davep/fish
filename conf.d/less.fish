34c2fa60a36678e0478e2d31abf94d557200907a
Dave Pearson <dave.pearson@synpromics.com>
2021-01-13 15:11:11 +0000

    :sparkles: Be more aggressive when we look for hilike-lesspipe

--- conf.d/less.fish
+++ conf.d/less.fish
@@ -9,11 +9,16 @@ if test -x /usr/bin/lesspipe
     set -x LESSOPEN "|/usr/bin/lesspipe %s"
 end
 
-# On the other hand, if we've got hilite...
+# On the other hand, if we've got hilite in /usr/...
 if test -x /usr/share/source-highlight/src-hilite-lesspipe.sh
     # ...use that instead.
     set -x LESS "$LESS"R
     set -x LESSOPEN "|/usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+else if type -fq src-hilite-lesspipe.sh
+    # Okay, so it wasn't where we'd often expect, but it is in the path, so
+    # let's go with that.
+    set -x LESS "$LESS"R
+    set -x LESSOPEN "|src-hilite-lesspipe.sh %s"
 end
 
 ### less.fish ends here
