df1afd365d4b4321949282dd9aa3e049647df389
Dave Pearson <dave.pearson@synpromics.com>
2022-08-04 14:49:20 +0100

    :sparkles: Add some handy Poetry-related abbreviations

--- conf.d/abbr.d/python.fish
+++ conf.d/abbr.d/python.fish
@@ -1,5 +1,12 @@
 ##############################################################################
 # Abbreviations related to Python stuff.
 abbr -g pylintshame "rg --no-messages \"pylint:disable=\" | awk 'BEGIN{FS=\"disable=\";}{print \$2}' | tr \",\" \"\n\" | sort | uniq -c | sort -hr"
+if type -q poetry; and status is-interactive;
+    abbr -g pe poetry;
+    abbr -g pes poetry shell;
+    abbr -g pei poetry init;
+    abbr -g per poetry run;
+    abbr -g pep poetry run python;
+end
 
 ### python.fish ends here
