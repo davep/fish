1d955c6a27f214f8753c558091306b3a073e8f38
Dave Pearson <davep@davep.org>
2022-10-18 09:02:22 +0100

    :art: Code tidy

--- conf.d/abbr.d/python.fish
+++ conf.d/abbr.d/python.fish
@@ -1,6 +1,8 @@
 ##############################################################################
 # Abbreviations related to Python stuff.
+
 abbr -g pylintshame "rg --no-messages \"pylint:disable=\" | awk 'BEGIN{FS=\"disable=\";}{print \$2}' | tr \",\" \"\n\" | sort | uniq -c | sort -hr"
+
 if type -q poetry; and status is-interactive;
     abbr -g pe poetry;
     abbr -g pes poetry shell;
