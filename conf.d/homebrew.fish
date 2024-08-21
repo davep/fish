7fe0af38fd2c93cfe154e2e03226abd1628a0aae
Dave Pearson <davep@davep.org>
2024-08-21 16:19:16 +0100

    :art: Clean up the Fish homebrew configuration

--- conf.d/homebrew.fish
+++ conf.d/homebrew.fish
@@ -1,28 +1,11 @@
 ##############################################################################
 # Things to make a Homebrew environment nicer.
 
-# If we're on macOS...
-if string match -q (uname) "Darwin"
-
-    # Ensure openssl library directory is seen by things.
-    set -l ossldir "/usr/local/opt/openssl@1.1"
-    if test -d $ossldir
-        add_path_maybe "$ossldir/bin"
-        set -agx LDFLAGS "-L$ossldir/lib/"
-        set -agx LIBRARY_PATH "$ossldir/lib/"
-        set -agx CPPFLAGS "-I$ossldir/include"
-        set -agx PKG_CONFIG_PATH "$ossldir/lib/pkgconfig"
-    end
-
-    # Ensure MySQL client library is seen by things.
-    if test -d "/usr/local/opt/mysql-client/"
-        add_path_maybe "/usr/local/opt/mysql-client/bin"
-        set -agx LDFLAGS "-L/usr/local/opt/mysql-client/lib"
-        set -agx LIBRARY_PATH "/usr/local/opt/mysql-client/lib"
-        set -agx CPPFLAGS "-I/usr/local/opt/mysql-client/include"
-        set -agx PKG_CONFIG_PATH "/usr/local/opt/mysql-client/lib/pkgconfig"
-    end
-
+# If homebrew seems to be installed (brew itself should be in my path
+# because I set that in 00_path.fish)...
+if type -fq brew
+    # ...do the rest of the shell environment setup.
+    brew shellenv | source
 end
 
 ### homebrew.fish ends here
