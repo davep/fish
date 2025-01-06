32ce316e0c6e4b4896ccc52afe59bab3642b1106
Dave Pearson <davep@davep.org>
2025-01-06 10:37:24 +0000

    :sparkles: Add an abbreviation for Braindrop

---
+++ conf.d/abbr.d/braindrop.fish
@@ -0,0 +1,9 @@
+##############################################################################
+# Abbreviations related to Braindrop
+
+# if https://github.com/davep/braindrop is installed.
+if type -q braindrop
+    abbr -g bd braindrop
+end
+
+### braindrop.fish ends here
