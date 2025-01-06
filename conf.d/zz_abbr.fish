d81050a986e3d4d5f8920d76dd2c59d8fe3883c2
Dave Pearson <davep@davep.org>
2025-01-06 10:53:51 +0000

    :hammer: Ensure abbreviations are loaded last

--- conf.d/zz_abbr.fish
+++ conf.d/zz_abbr.fish
@@ -4,9 +4,13 @@
 # Rather than keep them all in one file (which I was doing), this lets me
 # break them up a bit. I think it makes it far more tidy and easier to find
 # thing.
+#
+# Note the name of this file is an attempt to ensure that abbreviations are
+# loaded up after everything else; this means they can adapt to whatever
+# else is set in the environment.
 
 for abbr_file in ~/.config/fish/conf.d/abbr.d/*.fish
     source $abbr_file
 end
 
-### abbr.fish ends here
+### zz_abbr.fish ends here
