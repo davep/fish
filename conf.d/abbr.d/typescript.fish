094996f68116ec24aa6540b185db3dbdb2bc411d
Dave Pearson <davep@davep.org>
2025-03-20 14:33:08 +0000

    :sparkles: Add an abbreviation for running TypeScript code

---
+++ conf.d/abbr.d/typescript.fish
@@ -0,0 +1,9 @@
+##############################################################################
+# Abbreviations to help with TypeScript hacking.
+
+# If we have npx installed...
+if type -q npx
+    abbr -g tsx "npx tsx"
+end
+
+### typescript.fish ends here
