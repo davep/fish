4c940ff62a0aababeddae5ab8b43dc03cf07bd61
Dave Pearson <davep@davep.org>
2026-01-16 10:53:17 +0000

    :sparkles: Add an abbr for oldnews

---
+++ conf.d/abbr.d/oldnews.fish
@@ -0,0 +1,9 @@
+##############################################################################
+# Abbreviations related to Oldnews
+
+# if https://github.com/davep/oldnews is installed.
+if type -q oldnews
+    abbr -g on oldnews
+end
+
+### oldnews.fish ends here
