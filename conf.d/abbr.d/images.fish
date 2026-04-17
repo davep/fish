1a5315633b1d77ef0677895d69017d842c8ac92a
Dave Pearson <davep@davep.org>
2026-04-17 08:58:29 +0100

    :hammer: Rename mkwebp and add webpjpg

--- conf.d/abbr.d/images.fish
+++ conf.d/abbr.d/images.fish
@@ -2,7 +2,8 @@
 # Abbreviations for image manipulation commands.
 
 if type -q mogrify
-    abbr -g mkwebp "mogrify -format webp -define webp:lossless=true -quality 100"
+    abbr -g webppng "mogrify -format webp -define webp:lossless=true -quality 100"
+    abbr -g webpjpg "mogrify -format webp -quality 95"
 end
 
 ### images.fish ends here
