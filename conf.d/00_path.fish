977728c6a673a93f67c17adbe29385f6f922091c
Dave Pearson <davep@davep.org>
2026-06-07 19:30:09 +0100

    :sparkles: Add agy to the path

--- conf.d/00_path.fish
+++ conf.d/00_path.fish
@@ -5,6 +5,7 @@ fish_add_path --global --path ~/.cargo/bin
 fish_add_path --global --path ~/.local/bin
 fish_add_path --global --path ~/.roswell/bin
 fish_add_path --global --path ~/bin
+fish_add_path --global --path ~/.antigravity/antigravity/bin
 fish_add_path --global --path /usr/local/bin
 fish_add_path --global --path /usr/local/sbin
 fish_add_path --global --path /usr/X11/bin
