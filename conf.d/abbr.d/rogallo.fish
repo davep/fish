dd69aef3d512518c4302426b25797127a921dd91
Dave Pearson <davep@davep.org>
2026-08-04 16:56:02 +0100

    :sparkles: Add ab abbreviation for Rogallo

---
+++ conf.d/abbr.d/rogallo.fish
@@ -0,0 +1,9 @@
+##############################################################################
+# Abbreviations related to Rogallo.
+
+# If https://github.com/davep/rogallo is installed.
+if type -q rogallo
+    abbr -g rog rogallo
+end
+
+### rogallo.fish ends here
